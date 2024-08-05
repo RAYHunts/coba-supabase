b7059d725bd38de1fa349552f5a00bf69f1c12de
rayhunts <rayhunts710@gmail.com>
2024-08-05 21:48:27 +0700

    config prettier

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,18 @@
-import { login, signup } from "./actions";
+import { login, signup } from './actions';
 
 export default function LoginPage() {
   return (
     <form>
-      <div className="bg-indigo-500 flex flex-col">
+      <div className="flex flex-col bg-indigo-500">
         <label htmlFor="email">Email:</label>
         <input id="email" name="email" type="email" required className="text-indigo-950" />
         <label htmlFor="password">Password:</label>
         <input id="password" name="password" type="password" required className="text-indigo-950" />
         <div className="flex gap-4">
-          <button formAction={login} className="px-2 py-1 bg-lime-600 hover:bg-lime-700">
+          <button formAction={login} className="bg-lime-600 px-2 py-1 hover:bg-lime-700">
             Log in
           </button>
-          <button formAction={signup} className="px-2 py-1 bg-lime-600 hover:bg-lime-700">
+          <button formAction={signup} className="bg-lime-600 px-2 py-1 hover:bg-lime-700">
             Sign up
           </button>
         </div>
